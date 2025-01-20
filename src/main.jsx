eb41228bd89400177dd79297e09a28aebf387349
unknown <alazim-star>
2025-01-20 20:20:24 +0600

    add error page

--- src/main.jsx
+++ src/main.jsx
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
-    // errorElement:<ErrorPage></ErrorPage>,
+    errorElement:<ErrorPage></ErrorPage>,
     children:[
       {
         path: "/",
